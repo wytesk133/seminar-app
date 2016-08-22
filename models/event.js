82b095f190f0bdcdd6ab8b527dba08b90dd7325f
Waitaya Krongapiradee <wyte.sk133@gmail.com>
2016-08-22 10:13:39 +0700

    Parse event date properly

--- models/event.js
+++ models/event.js
@@ -3,6 +3,19 @@ var Model = require('../lib/model');
 class Event extends Model {
   static get typeName () { return 'event'; }
   static get designName () { return 'events'; }
+
+  save (callback) {
+    // parse date
+    var date = new Date(this.date);
+    var y = date.getFullYear();
+    var m = date.getMonth();
+    m ++;
+    if (m < 10) m = '0' + m;
+    var d = date.getDate();
+    if (d < 10) d = '0' + d;
+    this.date =  `${y}-${m}-${d}`;
+    super.save(callback);
+  }
 }
 
 module.exports = Event;
